adc977096d0b2bee69efbf27076ad1ebb76038db
MatiasBurecovics <111394291+MatiasBurecovics@users.noreply.github.com>
2023-08-30 12:08:02 -0300

    Add files via upload

--- src/pages/Estadisticas.js
+++ src/pages/Estadisticas.js
@@ -1,57 +1,56 @@
+import React, { useState, useEffect } from 'react';
 import Personas from './Listado';
 import './Estadisticas.css';
 
 export default function Estadisticas() {
-let mayoresEdad = 0;
-let personaMasGrande = [{edad: 0}]
-let personaMasChica = [{edad: 100}]
+  //falta que pasa si tienen la misma edad
+  const [mayoresEdad, setMayoresEdad] = useState(0);
+  const [personaMasGrande, setPersonaMasGrande] = useState({ edad: 0, nombre: '' });
+  const [personaMasChica, setPersonaMasChica] = useState({ edad: 100, nombre: '' });
 
-for (let i = 0; i < Personas.length; i++) {
-if (Personas[i].edad > 35) {
-mayoresEdad++;
-}
+  useEffect(() => {
+    let contadorMayoresEdad = 0;
+    let maxEdad = 0;
+    let minEdad = 100;
 
-if (personaMasGrande[0].edad < Personas[i].edad) {
-personaMasGrande = [Personas[i]]
-}
-else if (personaMasGrande[0].edad === Personas[i].edad) {
-personaMasGrande.push(Personas[i])
-}
+    Personas.forEach((persona) => {
+      if (persona.edad > 35) {
+        contadorMayoresEdad++;
+      }
 
-if (personaMasChica[0].edad > Personas[i].edad) {
-personaMasChica = [Personas[i]]
-}
-else if (personaMasChica[0].edad === Personas[i].edad) {
-personaMasChica.push(Personas[i])
-}
-}
+      if (persona.edad > maxEdad) {
+        maxEdad = persona.edad;
+        setPersonaMasGrande(persona);
+      }
 
-let textoGrande = "La persona más grande es: " + personaMasGrande[0].nombre;
-if (personaMasGrande.length > 1) {
-  textoGrande = "Las personas más grandes son: ";
-  personaMasGrande.forEach(Persona => {
-  textoGrande += Persona.nombre + ", "
-  });
-  textoGrande = textoGrande.slice(0, -2)
+      if (persona.edad < minEdad) {
+        minEdad = persona.edad;
+        setPersonaMasChica(persona);
+      }
+    });
+
+    setMayoresEdad(contadorMayoresEdad);
+  }, []);
+
+  let textoGrande = "La persona más grande es: " + personaMasGrande.nombre;
+
+  if (personaMasGrande.edad !== 0) {
+    textoGrande = personaMasGrande.nombre ? "La persona más grande es: " + personaMasGrande.nombre : "";
   }
-  
-  let textoChico = "La persona más chica es: " + personaMasChica[0].nombre;
-  
-  if (personaMasChica.length > 1) {
-  textoChico = "Las personas más chicas son: ";
-  personaMasChica.forEach(Persona => {
-  textoChico += Persona.nombre + ", "
-  });
-  textoChico = textoChico.slice(0, -2);
+
+  let textoChico = "La persona más chica es: " + personaMasChica.nombre;
+
+  if (personaMasChica.edad !== 100) {
+    textoChico = personaMasChica.nombre ? "La persona más chica es: " + personaMasChica.nombre : "";
   }
-  
+
   return (
     <div className="stats-container">
       <h1 className="stats-heading">Estadísticas de Personas</h1>
       <p className="stats-info">Cantidad de personas mayores de 35: {mayoresEdad}</p>
       <ul className="stats-person-list">
         <li className="stats-person-item">{textoGrande}</li>
-        <li className="stats-person-item">{textoChico}</li>
+        <li className="stats-person1-item">{textoChico}</li>
       </ul>
     </div>
   );
